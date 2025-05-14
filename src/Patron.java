d2c669f4bad834e935674e1afa29602ef8f5f930
Jena Mussleh <jmussleh@wppl.org>
2025-05-14 18:12:39 -0400

    Initial commit

---
+++ src/Patron.java
@@ -0,0 +1,44 @@
+//Attributes
+public class Patron {
+    private String ID;
+    private String Name;
+    private String Address;
+    private double Fines;
+
+    //Constructor
+    public Patron(String ID, String Name, String Address, double Fines) {
+        this.ID = ID;
+        this.Name = Name;
+        this.Address = Address;
+        this.Fines = Fines;
+    }
+
+    //Getter methods
+    public String getID() {
+        return ID;
+    }
+    public String getName() {
+        return Name;
+    }
+    public String getAddress() {
+        return Address;
+    }
+    public double getFines() {
+        return Fines;
+    }
+
+    //Setter methods
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+    public void setFines(double Fines) {
+        this.Fines = Fines;
+    }
+
+}
