7e9e0b07e787413f87d88883363b525a82bfee33
jmussleh <Jmussleh@gmail.com>
2025-05-14 19:41:02 -0400

    Added the loadFile function to read the fileName as a String. Also added the functionality that reads the texts and characters in the file and closes after completion.

--- src/Librarian.java
+++ src/Librarian.java
@@ -6,4 +6,13 @@ public class Librarian {
     //unique patron ID to the Patron object. The HashMap part allows
     //for quick adding and removing based on ID.
     private Map<String, Patron> patrons = new HashMap<>();
+
+    //Accepts a Sting for the fileName. The file closes automatically after
+    //being read. FileReader reads all characters in the file while bufferedReader
+    //reads the texts quickly.
+    public void loadFile(String fileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+        }
+    }
 }
