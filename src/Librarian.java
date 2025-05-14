af921e1595376961350a34e329dafe06d21f52b2
jmussleh <Jmussleh@gmail.com>
2025-05-14 19:23:55 -0400

    Added HashMap functionality to easily add and delete patrons based on ID

--- src/Librarian.java
+++ src/Librarian.java
@@ -1,2 +1,9 @@
+import java.io.*;
+import java.util.*;
+
 public class Librarian {
+    //Declares a map which stores the data in key value pairs. It maps the
+    //unique patron ID to the Patron object. The HashMap part allows
+    //for quick adding and removing based on ID.
+    private Map<String, Patron> patrons = new HashMap<>();
 }
